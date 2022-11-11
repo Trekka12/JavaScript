8a442d3b1c69f46812ccecba7ac6c6cef3246a92
Trekka12 <trekka12@cyberfox.se>
2022-11-11 14:02:52 +0100

    cleaned up repo of moved files

--- assign-event-handler-to-specific-elem-vanilla-js.js
+++
@@ -1,8 +0,0 @@
-// Below inspired from:
-// https://stackoverflow.com/questions/69516674/clearinterval-not-working-vanilla-javascript
-
-document.getElementById("your-elem-id-to-attach-event-listener-to").addEventListener("click", function() {
-	
-	// Do something once your-elem-id-to-attach-event-listener-to triggers a click event
-	
-}, false);
--- check-if-parent-has-child-elem-vanilla-js.js
+++
@@ -1,9 +0,0 @@
-// Below inspired from:
-// https://gomakethings.com/how-to-check-if-an-element-contains-another-one-with-the-vanilla-js-node.contains-method/
-
-let parentElem = document.getElementById("your-parent-elem-id-here");
-let childElem = document.getElementById("your-child-elem-id-here");
-
-if(parentElem.contains(childElem)) {
-	// Do something if the parent elem contained the child elem :)
-}

1df7e73e421069e5e713e800ba65d0db19739b4f
Trekka12 <trekka12@cyberfox.se>
2022-11-11 13:47:31 +0100

    Added yet another reference for this

--- check-elem-existence-vanilla-js.js
+++ check-elem-existence-vanilla-js.js
@@ -1,5 +1,6 @@
 // Below code is inspired from StackOverflow:
 // https://stackoverflow.com/questions/5629684/how-can-i-check-if-an-element-exists-in-the-visible-dom#answer-5629761
+// https://stackoverflow.com/questions/4186906/check-if-object-exists-in-javascript
 
 let elem = document.getElementById("your-elem-id-here");
 

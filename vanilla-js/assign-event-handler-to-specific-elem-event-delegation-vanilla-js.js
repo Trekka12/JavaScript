b24773e6311291695e510f0a4eed32e529b5d9f5
Trekka12 <trekka12@cyberfox.se>
2022-11-11 13:55:49 +0100

    Restructuring repo folder hierarchy and organization to future-proof

---
+++ vanilla-js/assign-event-handler-to-specific-elem-event-delegation-vanilla-js.js
@@ -0,0 +1,17 @@
+// Below inspired from:
+// https://stackoverflow.com/questions/26104525/best-practices-for-where-to-add-event-listeners
+// https://davidwalsh.name/event-delegate
+
+document.body.addEventListener("click", function(event) { // This will attach event listener to body and JS event delegation will "bubble" events through the DOM parents from elem until it reaches body where our listener is attached - this way we can have 1 listener for click instead of multiple
+	
+	// if our elem we want to act on got a class:
+	if(event.target.classList.contains("className")) {
+		// Act on your elem event trigger here
+	}
+	
+	// if our elem we want to act on got an id:
+	if(event.target.id == "your-elem-id-here") {
+		// Act on your elem event trigger here
+	}
+
+}, false);
---
+++ vanilla-js/assign-event-handler-to-specific-elem-vanilla-js.js
@@ -0,0 +1,8 @@
+// Below inspired from:
+// https://stackoverflow.com/questions/69516674/clearinterval-not-working-vanilla-javascript
+
+document.getElementById("your-elem-id-to-attach-event-listener-to").addEventListener("click", function() {
+	
+	// Do something once your-elem-id-to-attach-event-listener-to triggers a click event
+	
+}, false);

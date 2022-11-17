52415c8cd24577c53db8f781956857a3dae0617a
Trekka12 <trekka12@cyberfox.se>
2022-11-17 10:54:39 +0100

    Added code for checking if array contains value in Vanilla JS

---
+++ vanilla-js/check-if-value-exist-in-array-vanilla-js.js
@@ -0,0 +1,10 @@
+// Below uses the following references:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+const array = ["one", "two", "three"];
+
+if(array.includes("one")) {
+	
+	// if our array contained the textstring value "one" we do something here...
+	
+}

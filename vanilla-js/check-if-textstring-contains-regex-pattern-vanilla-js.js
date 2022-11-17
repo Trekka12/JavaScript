c2537663834ee83aa8f0ba3eb9ca5d12f71465e9
Trekka12 <trekka12@cyberfox.se>
2022-11-17 15:04:34 +0100

    corrected duplicate comment in script code example

--- vanilla-js/check-if-textstring-contains-regex-pattern-vanilla-js.js
+++ vanilla-js/check-if-textstring-contains-regex-pattern-vanilla-js.js
@@ -2,7 +2,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
 // https://regexr.com/
 

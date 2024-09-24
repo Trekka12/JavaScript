600d035edd9dd089be8549c57f3734cc7a2d3dad
Trekka12 <trekka12@cyberfox.se>
2024-09-24 05:31:23 +0200

    Update check-if-string-exist-in-array-or-string-func-js.js

--- vanilla-js/functions/check-if-string-exist-in-array-or-string-func-js.js
+++ vanilla-js/functions/check-if-string-exist-in-array-or-string-func-js.js
@@ -1,6 +1,5 @@
 /**
  * Super useful function to loop through an array to check vs RegEx if value of array slot match to regex (if array contains a string), if it is, return true, if not false, can also check string match, two-in-one combo
- * Searched Stackoverflow for something like this, but all the answers seemed way more complicated and convuluted - 23sept24.
  * @param arrOrStr  - array or str with value(s) to loop through for regex match
  * @param regex     - regex to match array slot values against
  * 
